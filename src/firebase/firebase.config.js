9cc78c39a21f86e521721ba11159334bf45b8363
iamnaymur <nrtusher1@gmail.com>
2023-04-30 12:37:16 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmJMWx8HuieqzSK3p0om_6YfKxQ27rKdU",
+  authDomain: "travel-guru-a9349.firebaseapp.com",
+  projectId: "travel-guru-a9349",
+  storageBucket: "travel-guru-a9349.appspot.com",
+  messagingSenderId: "1025231667162",
+  appId: "1:1025231667162:web:c18055be1a929a1701cae3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
